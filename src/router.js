cf339501ba4aa92d35f5e84ce264eebe8ffe7845
ank-21 <ankitSrivastava21598@gmail.com>
2020-05-03 05:04:52 +0530

    display added

--- src/router.js
+++ src/router.js
@@ -18,13 +18,45 @@ const dirPath = path.join(__dirname, "../");
 // });
 
 
+
+//console.log(targetFiles);
+    
+    // data.files = targetFiles; 
+
+const invoice = {
+    shipping: {
+        name: "John Doe",
+        address: "1234 Main Street",
+        city: "San Francisco",
+        state: "CA",
+        country: "US",
+        postal_code: 94111
+    },
+    items: [
+        {
+        item: "TC 100",
+        description: "Toner Cartridge",
+        quantity: 2,
+        amount: 6000
+        },
+        {
+        item: "USB_EXT",
+        description: "USB Cable Extender",
+        quantity: 1,
+        amount: 2000
+        }
+    ],
+    subtotal: 8000,
+    paid: 0,
+    invoice_nr: 1234
+};
+
+
 router.post('/data',(req,res)=>{
     console.log(req.body);
-    dataConverter(req.body);
+    dataConverter(invoice,req.body);
     const data = req.body;
-    // console.log(targetFiles);
     
-    // data.files = targetFiles;
     res.render('profile',{
         data
     })
